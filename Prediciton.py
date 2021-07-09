be71381c383646783374f1d70d2660e9c9e3e11c
yairsmadar <yairsemama@.com>
2021-07-09 11:16:34 +0300

    upload the project

---
+++ Prediciton.py
@@ -0,0 +1,298 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+# %matplotlib inline
+import nltk
+
+nltk.download('stopwords')
+plt.show()
+import numpy as np
+
+pd.set_option('display.max_columns', None)
+
+from nltk.corpus import stopwords
+
+stop_words = stopwords.words('english')
+from nltk.stem import WordNetLemmatizer
+import string
+
+import base64
+import string
+import re
+from collections import Counter
+from nltk.corpus import stopwords
+
+stopwords = stopwords.words('english')
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.base import TransformerMixin
+from sklearn.pipeline import Pipeline
+from sklearn.svm import LinearSVC
+from sklearn.feature_extraction._stop_words import ENGLISH_STOP_WORDS
+# from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
+from sklearn.metrics import accuracy_score, roc_auc_score, classification_report, confusion_matrix
+from nltk.corpus import stopwords
+import string
+import re
+import spacy
+
+spacy.load('en_core_web_md')
+
+from spacy.lang.en import English
+
+parser = English()
+
+df = pd.read_csv('Clean_data.csv')
+
+df.head()
+
+del df['Unnamed: 0']
+
+df.isnull().sum()
+
+df1 = df.copy()
+
+df1['fraudulent'].value_counts()
+
+import spacy
+
+nlp = spacy.load('en_core_web_lg')
+stopwords = stopwords.words('english')
+
+punctuations = string.punctuation
+
+
+#  look at the top 20 most common words being used in both fake postings as well as good job postings.
+def cleanup_text(docs, logging=False):
+    texts = []
+    counter = 1
+    for doc in docs:
+        if counter % 100 == 0 and logging:
+            print("Processed %d out of %d documents." % (counter, len(docs)))
+        counter += 1
+        doc = nlp(doc, disable=['parser', 'ner'])
+        tokens = [tok.lemma_.lower().strip() for tok in doc if tok.lemma_ != '-PRON-']
+        tokens = [tok for tok in tokens if tok not in stopwords and tok not in punctuations]
+        tokens = ' '.join(tokens)
+        texts.append(tokens)
+    return pd.Series(texts)
+
+
+# fake
+Fraud_1 = [text for text in df1[df1['fraudulent'] == 1]['description']]
+
+# Fraud_1[10]
+
+# real
+Fraud_0 = [text for text in df1[df1['fraudulent'] == 0]['description']]
+
+Fraud_1_clean = cleanup_text(Fraud_1)
+Fraud_1_clean = ' '.join(Fraud_1_clean).split()
+
+len(Fraud_1_clean)
+
+Fraud_0_clean = cleanup_text(Fraud_0)
+Fraud_0_clean = ' '.join(Fraud_0_clean).split()
+
+len(Fraud_0_clean)
+
+Fraud_1_counts = Counter(Fraud_1_clean)
+Fraud_0_counts = Counter(Fraud_0_clean)
+
+Fraud_1_common_words = [word[0] for word in Fraud_1_counts.most_common(20)]
+Fraud_1_common_counts = [word[1] for word in Fraud_1_counts.most_common(20)]
+
+#  display the most 20 common words in fake jobs
+fig = plt.figure(figsize=(20, 10))
+pal = sns.color_palette("cubehelix", 20)
+sns.barplot(x=Fraud_1_common_words, y=Fraud_1_common_counts, palette=pal)
+plt.title('Most Common Words used in Fake job postings')
+plt.ylabel("Frequency of words")
+plt.xlabel("Words")
+plt.show()
+
+Fraud_0_common_words = [word[0] for word in Fraud_0_counts.most_common(20)]
+Fraud_0_common_counts = [word[1] for word in Fraud_0_counts.most_common(20)]
+
+#  display the most 20 common words in real jobs
+fig = plt.figure(figsize=(20, 10))
+pal = sns.color_palette("cubehelix", 20)
+sns.barplot(x=Fraud_0_common_words, y=Fraud_0_common_counts, palette=pal)
+plt.title('Most Common Words used in Genuine job postings')
+plt.ylabel("Frequency of words")
+plt.xlabel("Words")
+plt.show()
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn import preprocessing
+from sklearn.preprocessing import OneHotEncoder
+
+import spacy
+
+# nlp = spacy.load('en_core_web_lg')
+nlp = spacy.load('en_core_web_sm')
+from nltk.corpus import stopwords
+
+stopwords = stopwords.words('english')
+
+STOPLIST = set(stopwords + list(ENGLISH_STOP_WORDS))
+SYMBOLS = " ".join(string.punctuation).split(" ")
+
+
+# get all words in the text
+def tokenizetext(sample):
+    text = sample.strip().replace("\n", " ").replace("\r", " ")
+    text = text.lower()
+    tokens = parser(text)
+    lemmas = []
+    for tok in tokens:
+        lemmas.append(tok.lemma_.lower().strip() if tok.lemma_ != "-PRON-" else tok.lower_)
+    tokens = lemmas
+    tokens = [tok for tok in tokens if tok not in STOPLIST]
+    tokens = [tok for tok in tokens if tok not in SYMBOLS]
+    return tokens
+
+
+# set hot vector to the most significant columns and clean all irrelevant columns
+vectorizer = CountVectorizer(tokenizer=tokenizetext, ngram_range=(1, 3), min_df=0.06)
+
+vectorizer_features = vectorizer.fit_transform(df1['description'])
+vectorizer_features.shape
+vectorizer_features
+vectorized_df = pd.DataFrame(vectorizer_features.todense(), columns=vectorizer.get_feature_names())
+df_vectorized = pd.concat([df1, vectorized_df], axis=1)
+
+df_vectorized.drop('description', axis=1, inplace=True)
+df_vectorized.dropna(inplace=True)
+
+df_vectorized.head()
+
+#  hot vector
+columns_to_1_hot = ['employment_type', 'required_experience', 'required_education', 'industry', 'function']
+
+for column in columns_to_1_hot:
+    encoded = pd.get_dummies(df_vectorized[column])
+    df_vectorized = pd.concat([df_vectorized, encoded], axis=1)
+
+columns_to_1_hot += ['title', 'city', 'country_name']
+df_vectorized.drop(columns_to_1_hot, axis=1, inplace=True)
+
+
+target = df_vectorized['fraudulent']
+features = df_vectorized.drop('fraudulent', axis=1)
+
+# split the data for train and test
+X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.1, stratify=target, random_state=42)
+
+Model_Score = []
+Model_Name = []
+
+#  Logistic Regression
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, roc_auc_score
+
+log_reg = LogisticRegression()
+c_values = [0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000]
+penalty_options = ['l1', 'l2']
+
+param_grid = dict(C=c_values, penalty=penalty_options)
+
+grid_tfidf = GridSearchCV(log_reg, param_grid=param_grid, cv=10, scoring='roc_auc', n_jobs=-1, verbose=1)
+
+grid_tfidf.fit(X_train, y_train)
+
+print(grid_tfidf.best_score_)
+
+print(grid_tfidf.best_params_)
+
+log_reg_pred = grid_tfidf.predict(X_test)
+
+roc_auc_score(y_test, log_reg_pred)
+
+print(classification_report(y_test, log_reg_pred))
+
+Model_Name.append('Logistic_Regression')
+Model_Score.append(round(roc_auc_score(y_test, log_reg_pred), 4))
+
+
+#  K-Nearest Neighbors (KNN)
+knn = KNeighborsClassifier()
+k_range = list(np.arange(2, 23, 2))
+param_grid_knn = dict(n_neighbors=k_range)
+print(param_grid_knn)
+
+grid_knn = GridSearchCV(knn, param_grid_knn, cv=10, scoring='roc_auc', n_jobs=-1, verbose=1)
+
+grid_knn.fit(X_train, y_train)
+
+grid_knn.best_score_
+
+grid_knn.best_params_
+
+knn_pred = grid_knn.predict(X_test)
+
+print(roc_auc_score(y_test, knn_pred))
+print(classification_report(y_test, knn_pred))
+
+Model_Name.append('KNN')
+Model_Score.append(round(roc_auc_score(y_test, knn_pred), 4))
+
+
+#  Support Vector Machines (SVM)
+svc = SVC()
+kernel = ['linear', 'rbf']
+param_grid_knn = dict(kernel=kernel)
+print(param_grid_knn)
+
+grid_svc = GridSearchCV(svc, param_grid_knn, cv=10, scoring='roc_auc', n_jobs=-1, verbose=2)
+
+grid_svc.fit(X_train, y_train)
+
+grid_svc.best_score_
+
+grid_svc.best_params_
+
+svc_pred = grid_svc.predict(X_test)
+
+print(roc_auc_score(y_test, svc_pred))
+print(classification_report(y_test, svc_pred))
+
+Model_Name.append('SVC')
+Model_Score.append(round(roc_auc_score(y_test, svc_pred), 4))
+
+
+# Random Forest
+rf = RandomForestClassifier()
+n_estimators_range = [1, 2, 4, 8, 16, 32, 64, 100, 200]
+param_grid_rf = dict(n_estimators=n_estimators_range)
+grid_rf = GridSearchCV(rf, param_grid_rf, cv=10, scoring='roc_auc', n_jobs=-1, verbose=1)
+
+grid_rf.fit(X_train, y_train)
+print(grid_rf.best_score_)
+print(grid_rf.best_params_)
+
+rf_pred = grid_rf.predict(X_test)
+print(roc_auc_score(y_test, rf_pred))
+print(classification_report(y_test, rf_pred))
+
+Model_Name.append('Random_Forest')
+Model_Score.append(round(roc_auc_score(y_test, rf_pred), 4))
+
+plt.figure(figsize=(15, 10))
+plt.plot(Model_Name, Model_Score, marker='o', color='red')
+plt.title('Comparison of different models')
+plt.xlabel('Models')
+plt.ylabel('ROC-AUC score')
+plt.ylim(0.5, 1.0)
+plt.grid()
+plt.savefig('Model_compare.jpeg')
+plt.show()
